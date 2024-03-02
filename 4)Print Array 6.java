7c20d5ee1e9dd5df2e96b984cd82ec6cba4c74a7
Logesh <84928658+IVAR23-NITRO@users.noreply.github.com>
2024-03-03 00:49:28 +0530

    Create 4)Print Array 6.java

---
+++ 4)Print Array 6.java
@@ -0,0 +1,17 @@
+        Scanner in = new Scanner(System.in);
+               
+           
+               int size = in.nextInt();
+               
+               int[] a = new int[size];
+               
+    
+               for (int i = 0; i < size; i++) {
+                   a[i] = in.nextInt();
+              }
+            //int []a= {1,2,3,4,5,6,7,8};
+            
+            for (int j=0;j<a.length;j++) {
+                System.out.print(a[j]+" ");
+            
+    }
